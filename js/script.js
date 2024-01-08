85890623d19512514f65eddb152c28b74a7f68c3
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-08 14:49:21 +0100

    add console shopping list output

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK');
+
+const groceryList = ["Pane", "Latte", "Uova", "Pollo", "Pomodori", "Pasta", "Frutta", "Formaggio", "Yogurt", "Cereali"];
+
+console.log(groceryList);
+
+let i = 0;
+
+while (i < groceryList.length) {
+    const listElement = groceryList[i];
+    console.log("Elemento lista:", listElement);
+    i++;
+}
